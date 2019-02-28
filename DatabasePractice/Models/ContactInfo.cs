eb542d9584e6ea8a0cfeed9817ecfb4499c490b3
Alden Molina <ajd029@yahoo.com>
2019-02-28 10:29:47 -0500

    Added models and classroom context

---
+++ DatabasePractice/Models/ContactInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabasePractice.Models
+{
+    public class ContactInfo
+    {
+        public int ContactId { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ DatabasePractice/Models/Subject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabasePractice.Models
+{
+    public class Subject
+    {
+    }
+}
